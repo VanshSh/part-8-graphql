93b992325105e3682882e7ad5a0ade727c67e296
VanshSharmaS7C <vansh.sharma@seventhtriangle.com>
2023-09-01 23:50:14 +0530

    feat: :sparkles: Exercise: 8.4 => Books of an author

--- library-backend.js
+++ library-backend.js
@@ -93,7 +93,7 @@ const typeDefs = `
 type Query {
   bookCount: Int!
   authorCount: Int!
-  allBooks: [Book!]!
+  allBooks(author:String!): [Book]
   allAuthors: [Author!]!
 }
 
@@ -115,7 +115,9 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      return books.filter((book) => book.author === args.author)
+    },
     allAuthors: () => {
       return authors.map((author) => ({
         name: author.name,
